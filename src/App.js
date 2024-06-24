914a4ce5688eff1e1c3a303c40ef6ac86b84585b
Sandy Abasman <abasifrekesandy@gmail.com>
2024-06-24 17:38:15 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./style.css";
+
+const faq = [
+  {
+    text: "What is your name?",
+    answer: "My name is Sandy",
+  },
+
+  {
+    text: "How old are you?",
+    answer: "old enough",
+  },
+
+  {
+    text: "What are you learning?",
+    answer: "web development",
+  },
+
+  {
+    text: "What is your nationality?",
+    answer: " Nigerian",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <QuestionContainer data={faq} />;
+    </div>
+  );
+}
+
+function QuestionContainer({ data }) {
+  return (
+    <div className="question-container">
+      {data.map((item, index) => (
+        <Question
+          text={item.text}
+          answer={item.answer}
+          number={index}
+          key={index}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Question({ text, answer, number }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+  return (
+    <div
+      className={`each-question ${isOpen && "selected"}`}
+      onClick={handleToggle}
+    >
+      <div className="upper-question">
+        <p className="number">{number < 9 ? ` 0${number + 1}` : number + 1}</p>
+        <p className="text">{text}</p>
+        <p className="icon">{isOpen ? "-" : "+"}</p>
+      </div>
+      {isOpen && (
+        <div className="answer">
+          <p>{answer}</p>
+        </div>
+      )}
     </div>
   );
 }
